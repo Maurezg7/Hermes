64ed8761af42b9658af7744299670a7305f7df6d
Maurezg7 <mauroleonelg7@gmail.com>
2026-05-23 00:46:53 -0300

    Eliminada la carpeta frontend de la rama frontend

--- backend/src/main/java/maurezg7/backend/models/Enum/FriendshipStatus.java
+++
@@ -1,5 +0,0 @@
-package maurezg7.backend.models.Enum;
-
-public enum FriendshipStatus {
-    PENDING, ACCEPTED, BLOCKED
-}
--- backend/src/main/java/maurezg7/backend/models/Enum/StatesUser.java
+++
@@ -1,5 +0,0 @@
-package maurezg7.backend.models.Enum;
-
-public enum StatesUser {
-    LINE, INACTIVE, NOT_DISTURB,  DISCONNECTED
-}
